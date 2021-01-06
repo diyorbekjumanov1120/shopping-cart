6f423f52e99dfdf1de01d17185432795741095f5
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-06 17:54:12 +0000

    the space was also corrected

--- shop-cart-2.py
+++ shop-cart-2.py
@@ -13,7 +13,7 @@ for half_data in lst:
     idx2 = half_data.find('"', idx1)
     itemPrice = float(half_data[idx1:idx2])
 
-    idx1 = half_data.find('quantity') + 11
+    idx1 = half_data.find('quantity') + 10
     idx2 = half_data.find('}', idx1)
     quantity = float(half_data[idx1:idx2])
 
