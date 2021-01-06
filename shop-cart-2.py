8f381bc41de8c107a6f5ade83b04a9dfe1ad828c
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-06 16:49:47 +0000

    The total cost of the purchase was determined

--- shop-cart-2.py
+++ shop-cart-2.py
@@ -16,3 +16,5 @@ for half_data in lst:
     idx1 = half_data.find('quantity') + 11
     idx2 = half_data.find('}', idx1)
     quantity = float(half_data[idx1:idx2])
+
+    total += itemPrice * quantity
