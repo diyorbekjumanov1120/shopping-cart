56016ef43387d013dfbdf17f4fdcb3bd34fac37d
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-06 16:48:38 +0000

    It has been determined how many were retrieved from ItemName

--- shop-cart-2.py
+++ shop-cart-2.py
@@ -12,3 +12,7 @@ for half_data in lst:
     idx1 = half_data.find('$') + 1
     idx2 = half_data.find('"', idx1)
     itemPrice = float(half_data[idx1:idx2])
+
+    idx1 = half_data.find('quantity') + 11
+    idx2 = half_data.find('}', idx1)
+    quantity = float(half_data[idx1:idx2])
